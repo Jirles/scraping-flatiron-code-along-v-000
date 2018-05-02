32187f7130327b4ea6e0d49780e06a199677372f
Emily Jirles <emily.jirles@gmail.com>
2018-05-02 19:33:04 +0000

    automatically backed up by learn

--- lib/course.rb
+++ lib/course.rb
@@ -1,8 +1,15 @@
 class Course
   
   attr_accessor :title, :schedule, :description
+  @@all = [] 
   
+  def initialize
+    self.class.all << self 
+  end
   
+  def self.all 
+    
+  end
   
 end
 
